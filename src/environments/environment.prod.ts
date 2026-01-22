6c11178315bb7fd3fd7230c6ca5b186d0df667c3
Javier <jrenzullis@gmail.com>
2026-01-22 14:25:22 +0100

    Configuracion inicial del proyecto y variables de entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  openWeatherApiKey: '85aef2b8f19e76d4653b8255a8a6566a',
+  openWeatherUrl: 'https://api.openweathermap.org/data/2.5'
 };
